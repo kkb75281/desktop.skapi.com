bdb8e22a87f096fdfaa6d51fd0f437038f615e3f
gyupp <kkb75281@gmail.com>
2024-01-24 12:36:37 +0900

    change

--- src/views/Service/Records/DeleteRecordOverlay.vue
+++ src/views/Service/Records/DeleteRecordOverlay.vue
@@ -2,7 +2,9 @@
 #overlayWindow.deleteRecord(@click="emits('close')" style='--max-width: 376px;')
     .overlayWrap(@click.stop)
         header
-            h5.title.red Delete Record
+            .title.withIcon.red
+                .material-symbols-outlined.mid.icon warning
+                h5 Delete Record
         main
             .content
                 p You sure want to delete {{ toDelete.count > 1 ? toDelete.count + ' records' : 'the record'}}?
@@ -13,8 +15,8 @@
                     //- template(v-if="promiseRunning")
                     //-     img.loading(src="@/assets/img/loading.png")
                     //- template(v-else)
-                    button.cancel(@click="emits('close')") Cancel
-                    button.save(@click="toDelete.cb();emits('close')") Delete
+                    button.noLine(@click="emits('close')") Cancel
+                    button.unFinished.warning(@click="toDelete.cb();emits('close')") Delete
 </template>
 
 <script setup>
