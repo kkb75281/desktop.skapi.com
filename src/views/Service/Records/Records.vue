6d066377999b73ad09c1d4260e348ef812bd3642
gyupp <kkb75281@gmail.com>
2024-01-29 16:14:31 +0900

    change

--- src/views/Service/Records/Records.vue
+++ src/views/Service/Records/Records.vue
@@ -1129,7 +1129,7 @@ watch(() => selectedRecord.value, () => {
         background-color: #fafafa;
         border-radius: 8px;
         margin-bottom: 2%;
-        box-shadow: 8px 12px 36px rgba(0, 0, 0, 0.10);
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
         overflow: hidden;
     }
 
--- src/views/Service/Users/Users.vue
+++ src/views/Service/Users/Users.vue
@@ -628,7 +628,7 @@ onMounted(() => {
         background-color: #fafafa;
         border-radius: 8px;
         margin-bottom: 2%;
-        box-shadow: 8px 12px 36px rgba(0, 0, 0, 0.10);
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
     }
 
     form {
